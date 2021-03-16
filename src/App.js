6a97e91568571130d6450be1b93cbab237a5ff47
Khaled Khalil <khaled.m.hassan.94@gmail.com>
2021-03-16 14:50:45 +0200

    fatboy pis

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-// import logo from './logo.svg';
+import logo from "./logo.svg";
 import "./App.css";
+import BikeCard from "./components/BikeCard";
 
 function App() {
   return (
     <div className="App">
       <header className="container">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src={logo} className="App-logo" alt="logo" />
+
+        {/* Page Title */}
+        <div className="page_title">
+          <h1>Bikes in San Francisco</h1>
+        </div>
+
+        {/* Bikes Cards Component*/}
+        <BikeCard></BikeCard>
       </header>
     </div>
   );
