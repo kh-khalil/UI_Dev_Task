bee8764c5a0dc775e1303281a8ee865d8d1c14da
Khaled Khalil <khaled.m.hassan.94@gmail.com>
2021-03-18 14:45:17 +0200

    added button but not working

--- src/screens/MobileScreen.js
+++ src/screens/MobileScreen.js
@@ -2,6 +2,12 @@ import React from "react";
 import BikeCard from "../components/BikeCard";
 
 export default function MobileScreen() {
+  // const otherRentersAction = () => {
+  //   document.getElementById("overlay").style.display = "block";
+  // };
+  // const off = () => {
+  //   document.getElementById("overlay").style.display = "none";
+  // };
   return (
     <>
       <div className="page_title text-center">
@@ -17,7 +23,14 @@ export default function MobileScreen() {
         ></BikeCard>
 
         {/* Other Renters */}
-        <div className="other_renters_container text-left">
+        {/* <div type="button" id="overlay" onclick={off}>
+          <div id="text">Overlay Text</div>
+        </div> */}
+        <div
+          // type="button"
+          // onClick={otherRentersAction}
+          className="other_renters_container text-left"
+        >
           <div className="other_renters_label d-flex">
             <i class="fas fa-bars "></i>
             <p className="other_renters_number">6 Other Renters</p>
@@ -25,6 +38,12 @@ export default function MobileScreen() {
           </div>
         </div>
       </div>
+
+      {/* 
+
+                                     second Card 
+      
+      */}
 
       {/* Card Container */}
       <div className="p-0 card_container text-center">
@@ -35,13 +54,13 @@ export default function MobileScreen() {
         ></BikeCard>
 
         {/* Other Renters */}
-        <div className="other_renters_container text-left">
+        <button type="button" className="other_renters_container text-left">
           <div className="other_renters_label d-flex">
             <i class="fas fa-bars "></i>
             <p className="other_renters_number">6 Other Renters</p>
             <p className="other_renters_price">from $120 /day</p>
           </div>
-        </div>
+        </button>
       </div>
     </>
   );
